3ea759db269a5a040965c52acd42d4dc50287644
MariaGabrielly22 <splendoregabrielly222@gmail.com>
2025-03-13 09:42:24 -0300

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.getElementById('btnquery').addEventListener('click', function() {
+    let circulo = document.querySelector(".circulos .circulo");
+    circulo.classList.toggle("ativo");
+});
