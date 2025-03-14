f451d0d260ec83084f40ead44ec0515255afaa6e
MariaGabrielly22 <splendoregabrielly222@gmail.com>
2025-03-14 08:57:49 -0300

    Update dom.js

--- dom.js
+++ dom.js
@@ -2,3 +2,13 @@ document.getElementById('btnquery').addEventListener('click', function() {
     let circulo = document.querySelector(".circulos .circulo");
     circulo.classList.toggle("ativo");
 });
+
+document.getElementById('btnqueryAll').addEventListener('click', function() {
+    let todosCirculos = document.querySelectorAll('.circulo');
+    tudoAtivos = true;
+    tudoAtivos = Array.from(todosCirculos).every(el => el.classList.contains('ativo'));
+    
+    todosCirculos.forEach(cipc => {
+        cipc.classList.toggle("ativo", !tudoAtivos);
+    });
+});
